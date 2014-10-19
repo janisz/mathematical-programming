1c1264e0449cbbd12fdab4069427dd494cf640ef
Tomasz Janiszewski <janiszt@gmail.com>
2014-10-19 18:01:09 +0200

    wget -A m -r -l 1 -nd http://www.hkn.umn.edu/resources/files/matlab/tutorial/Tut6_Files

--- simplex.m
+++ simplex.m
@@ -1,23 +1,22 @@
 
-function[A, subs, x, z] = simplex(type, c, A, b, varargin);
+function[A, subs, x, z] = simplex(type, c, A, b);
 
 % The simplex algorithm for the LP problem
-
+%
 %                    min(max) z = c*x
 %                 Subject to: Ax <= b
 %                              x >= 0
-
+%
 % Vector b must be nonnegative.
 % For the minimization problems the string type = 'min',
 % otherwise type = 'max'.
-% The fifth input parameter is optional. If it is set to 'y',
-% then the initial and final tableaux are displayed to the
-% screen.
 % Output parameters:
 % A - final tableau of the simplex method
 % subs - indices of the basic variables in the final tableau
 % x - optimal solution
 % z - value of the objective function at x.
+%
+% See also linprog
 
 if any(b < 0)
    error(' Right hand sides of the constraint set must be nonnegative.')
@@ -35,7 +34,7 @@ c = c(:)';
 A = [A b];
 d = [c zeros(1,m+1)];
 A = [A;d];
-if nargin == 5
+
    disp(sprintf('     ________________________________________________'))
    disp(sprintf('\n              Tableaux of the Simplex Algorithm'))
    disp(sprintf('     ________________________________________________'))
@@ -43,10 +42,10 @@ if nargin == 5
    A
    disp(sprintf(' Press any key to continue ...\n\n'))
    pause
-end
+
 [mi, col] = Br(A(m+1,1:m+n));
 subs = n+1:m+n;
-while ~isempty(mi) & mi < 0 & abs(mi) > eps
+while (~isempty(mi) & mi < 0 & abs(mi) > eps)
    t = A(1:m,col);
    if all(t <= 0)
       disp(sprintf('\n       Problem has unbounded objective function'));
@@ -74,9 +73,8 @@ z = tp*A(m+1,m+n+1);
 x = zeros(1,m+n);
 x(subs) = A(1:m,m+n+1);
 x = x(1:n)';
-if nargin == 5
+
    disp(sprintf('\n\n                  Final tableau'))
    A
    disp(sprintf(' Press any key to continue ...\n'))
    pause
-end
