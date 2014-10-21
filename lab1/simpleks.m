a2acde61987f81eda38aa1629ffe49e098dc561c
Tomasz Janiszewski <janiszt@gmail.com>
2014-10-21 21:32:31 +0200

    Add exitflag

--- lab1/simpleks.m
+++ lab1/simpleks.m
@@ -12,8 +12,18 @@ m = m+n;
 n = 2*n;
 [mi, col] = min(A(m+1,1:m+n));
 base = n+1:m+n
+
+if any(b < 0)
+   exitflag = 0
+end
+
 while (~isempty(mi) & mi < 0 & abs(mi) > eps)
    t = A(1:m,col);
+   if all(t <= 0)
+      x = zeros(n,1)
+      exitflag = 0;
+      return;
+   end
    row = MinimumRatio(A(1:m,m+n+1),A(1:m,col));
    if ~isempty(row)
       A(row,:)= A(row,:)/A(row,col);
@@ -31,12 +41,11 @@ x = zeros(1,m+n);
 x(base) = A(1:m,m+n+1);
 x = x(1:n)';
 x = x(1:length(x)/2) - x(length(x)/2+1:end)
+exitflag = 1
 
-function [row, mi] = MinimumRatio(a, b)
+function row = MinimumRatio(a, b)
 m = length(a);
 c = 1:m;
-a = a(:);
-b = b(:);
 l = c(b > 0);
 [mi, row] = min(a(l)./b(l));
 row = l(row);
