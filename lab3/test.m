c8ce8e3c90fd9cd359b45f5dc2f8d5768cd78838
Tomasz Janiszewski <janiszt@gmail.com>
2014-11-16 18:16:21 +0100

    Fix typo

--- lab3/test.m
+++ lab3/test.m
@@ -1,6 +1,4 @@
-[a1, a2, a3] = przedzial_niepewnosci(@(x) x^2, 1)
-assert([a1 ,a2, a3], [1, 1.5, 2])
-[a1, a2, a3] = przedzial_niepewnosci(@(x) sin(x), -1)
-assert([a1 ,a2, a3], [-1, -0.5, 0])
 [a1, a2, a3] = przedzial_niepewnosci(@(x) x^2, -1)
 assert([a1 ,a2, a3], [-1, 0, 1])
+[a1, a2, a3] = przedzial_niepewnosci(@(x) sin(x), -1, 1.5)
+assert([a1 ,a2, a3], [-1, -0.5, 0])
