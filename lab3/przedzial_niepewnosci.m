5e02a35f026668d9b117e3172742e4ce347337ee
Tomasz Janiszewski <janiszt@gmail.com>
2014-11-16 18:10:30 +0100

    Fix calculation error

--- lab3/przedzial_niepewnosci.m
+++ lab3/przedzial_niepewnosci.m
@@ -11,13 +11,13 @@ punktProbny = a1 + delta;
 if (F(a1) <= F(punktProbny))
   a3 = punktProbny
   a2 = (a1+a3)/2
-  while (warunke3P(F, a1, a2 , a3) == 1)
+  while (warunke3P(F, a1, a2 , a3) != 1)
     a2 = (a1+a2)/2
   end
 else
   a2 = punktProbny
   a3 = a2 + delta
-  while (~warunke3P(F, a1, a2 , a3))
+  while (warunke3P(F, a1, a2 , a3) != 1)
     delta = 2*delta
     a3 = a2 + delta
   end
@@ -25,5 +25,4 @@ end
 
 
 function [spelnia] = warunke3P(F, a1, a2, a3)
-
-spelnia = ((a1 < a2 && a2 < a3) && (F(a1) > F(a2) && F(a3) > F(a2)))
+spelnia = (a1 < a2 && a2 < a3 && F(a1) > F(a2) && F(a3) > F(a2))
